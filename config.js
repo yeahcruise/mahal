a7962a6b7db6396bb9ee6fc407f2c1f9eda1074a
yeahcruise <danielchan.cruz@gmail.com>
2025-02-12 22:10:56 -0800

    Update config.js

--- config.js
+++ config.js
@@ -42,9 +42,9 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "WOOOOW PARANG YOU LOVE ME MOST?? 🥰💓🦋",  // Shows when they go past 5000%
+        high: "To saturn and back! 🦋🪐💓",              // Shows when they go past 1000%
+        normal: "sobraaaa! 🥰🦋"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
